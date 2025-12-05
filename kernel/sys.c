0784f19a76be103417f3f3e915baf4fe4a78ba51
ApartTUSITU <aptusitu@outlook.com>
2025-12-05 21:45:42 +0800

    fixup! syscall: Increase bpf fake uname to 5.10.240

--- kernel/sys.c
+++ kernel/sys.c
@@ -1246,6 +1246,14 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4) ||
+	    !strncmp(current->comm, "uprobestats", 11)) {
+		strcpy(tmp.release, "5.10.240");
+		pr_debug("fake uname: %s release=%s\n",
+			 current->comm, tmp.release);
+	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
@@ -1270,14 +1278,6 @@ SYSCALL_DEFINE1(uname, struct old_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "bpfloader", 9) ||
-	    !strncmp(current->comm, "netbpfload", 10) ||
-	    !strncmp(current->comm, "netd", 4) ||
-	    !strncmp(current->comm, "uprobestats", 11)) {
-		strcpy(tmp.release, "5.10.240");
-		pr_debug("fake uname: %s release=%s\n",
-			 current->comm, tmp.release);
-	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
